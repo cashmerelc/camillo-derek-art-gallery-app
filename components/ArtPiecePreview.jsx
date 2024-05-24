38f97d91c3525b482485a339ac8e19f0609c190a
Derek Myers <cashmerelc@gmail.com>
2024-05-24 16:32:48 +0200

    Add resources and introduce artPiecesInfo to store the isFavorite flag

--- components/ArtPiecePreview.jsx
+++ components/ArtPiecePreview.jsx
@@ -5,7 +5,14 @@ export function ArtPiecePreview({ image, title, artist }) {
     <>
       <div className="art-piece-image-container">
         <Image src={image} alt={title} width={200} height={200} />
-        <button className="favorite-button">❤️</button>
+        <button className="favorite-button">
+          <Image
+            src="../assets/heart.svg"
+            alt="Favorite"
+            width={10}
+            height={10}
+          />
+        </button>
       </div>
 
       <h2>
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   const [artPieces, setArtPieces] = useState([]);
+  const [artPiecesInfo, setArtPiecesInfo] = useState([]);
 
   const fetcher = async (url) => {
     const res = await fetch(url);
@@ -23,7 +24,7 @@ export default function App({ Component, pageProps }) {
     "https://example-apis.vercel.app/api/art",
     fetcher
   );
-
+  console.log(data);
   useEffect(() => {
     if (data) {
       setArtPieces(data);
@@ -33,11 +34,33 @@ export default function App({ Component, pageProps }) {
   if (error) return <div>There was an error fetching the art pieces...</div>;
   if (isLoading) return <div>Loading...</div>;
 
+  function handleToggleFavorite(slug) {
+    // See if the art piece is already in the array
+    const info = artPiecesInfo.find((art) => art.slug === slug);
+    if (info) {
+      // If the art piece is already in the array, toggle the isFavorite value
+      const newInfo = artPiecesInfo.map((info) => {
+        info.slug = slug
+          ? { ...info, isFavorite: !info.isFavorite }
+          : info.isFavorite;
+      });
+      setArtPiecesInfo(newInfo);
+    }
+    // If the art piece is not in the array already, add it with the favorite as true
+    const newInfo = [...artPiecesInfo, { slug, isFavorite: true }];
+    setJokesInfo(newInfo);
+  }
+
   return (
     <>
       <GlobalStyle />
       {artPieces.length > 0 ? (
-        <Component {...pageProps} pieces={artPieces} />
+        <Component
+          {...pageProps}
+          pieces={artPieces}
+          artPiecesInfo={artPiecesInfo}
+          onFavorite={handleToggleFavorite}
+        />
       ) : (
         <div>Loading...</div>
       )}
