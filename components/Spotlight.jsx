6f62bc2f9806bd6afe4e139b2b8fc176088ccb81
Camillo Hochberg <camillo.h@hotmail.de>
2024-05-24 13:00:35 +0200

    add Spotlight component

--- components/Spotlight.jsx
+++ components/Spotlight.jsx
@@ -1,17 +1,20 @@
-import { ArtPiecePreview } from "./ArtPiecePreview";
-
+import Image from "next/image";
 export function Spotlight({ pieces }) {
   const SpotlightSelection = pieces[Math.floor(Math.random() * pieces.length)];
-
-  console.log(SpotlightSelection);
   return (
     <ul>
-      <li key={SpotlightSelection.slug}>
-        <ArtPiecePreview
-          image={SpotlightSelection.imageSource}
-          title={SpotlightSelection.name}
-          artist={SpotlightSelection.artist}
-        />
+      <li>
+        <div className="art-piece-image-container">
+          <Image
+            src={SpotlightSelection.imageSource}
+            alt={SpotlightSelection.name}
+            width={200}
+            height={200}
+          />
+          <button className="favorite-button">❤️</button>
+        </div>
+
+        <h2> {SpotlightSelection.artist}</h2>
       </li>
     </ul>
   );
