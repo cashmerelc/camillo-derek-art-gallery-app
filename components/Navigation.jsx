fa21af5d4dcb433a1bf3eacaca899e26da77b957
Camillo Hochberg <camillo.h@hotmail.de>
2024-05-28 13:43:49 +0200

    fix vercel deployment error (a > Link)

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,46 +2,18 @@ import Link from "next/link";
 
 import styled from "styled-components";
 
-const StyledNavBar = styled.nav`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  font-size: 1rem;
-  background-color: #ccc;
-  align-items: center;
-`;
-
-const StyledNavList = styled.ul`
-  display: flex;
-  list-style-type: none;
-  justify-content: space-between;
-`;
-
-const StyledNavItem = styled.li`
-  width: 30%;
-  height: 40px;
-  text-decoration: none;
-  justify-content: space-around;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: #000;
-`;
-
 export function Navigation() {
   return (
     <nav>
       <ul>
         <li>
-          <a href="/">Spotlight</a>
+          <Link href="/">Spotlight</Link>
         </li>
         <li>
-          <a href="/art-pieces">Art Pieces</a>
+          <Link href="/art-pieces">Art Pieces</Link>
         </li>
         <li>
-          <a href="/favorites">Favorites</a>
+          <Link href="/favorites">Favorites</Link>
         </li>
       </ul>
     </nav>
