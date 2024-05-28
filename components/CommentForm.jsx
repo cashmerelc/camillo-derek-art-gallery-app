c0d603498c1c0b8d3b771a94f7ab9020e3daa5ec
Derek Myers <cashmerelc@gmail.com>
2024-05-28 10:37:46 +0200

    Fix issue that allows empty comments to be added

--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -4,6 +4,23 @@ const StyledFormDiv = styled.div`
   margin-bottom: 125px;
 `;
 
+// export function CommentForm({ slug, onSubmitComment }) {
+//   return (
+//     <StyledFormDiv>
+//       <label htmlFor="add-comment">Add comment:</label>
+//       <input type="text" id="add-comment" name="add-comment"></input>
+//       <button
+//         name="submit-comment"
+//         onClick={() =>
+//           onSubmitComment(slug, document.getElementById("add-comment").value)
+//         }
+//       >
+//         Send
+//       </button>
+//     </StyledFormDiv>
+//   );
+// }
+
 export function CommentForm({ slug, onSubmitComment }) {
   return (
     <StyledFormDiv>
@@ -11,9 +28,14 @@ export function CommentForm({ slug, onSubmitComment }) {
       <input type="text" id="add-comment" name="add-comment"></input>
       <button
         name="submit-comment"
-        onClick={() =>
-          onSubmitComment(slug, document.getElementById("add-comment").value)
-        }
+        onClick={() => {
+          if (document.getElementById("add-comment").value === "") {
+            alert("Comment input cannot be empty");
+          } else {
+            onSubmitComment(slug, document.getElementById("add-comment").value);
+            document.getElementById("add-comment").value = "";
+          }
+        }}
       >
         Send
       </button>
